1cacacfe29b044b0c2d4ab27c9ef94e88bb01a9b
Prashant174 <yprashant174@gmail.com>
2023-01-08 00:08:26 +0530

    try1

---
+++ script/index.js
@@ -0,0 +1,20 @@
+
+
+
+// console.log("hello")
+
+document.querySelector(".cross").style.display="none"
+document.querySelector(".hemburger").addEventListener("click",()=>{
+    document.querySelector(".sidebar").classList.toggle("sidebargo")
+    if(document.querySelector(".sidebar").classList.contains("sidebargo")){
+        document.querySelector(".hem").style.display='inline'
+        document.querySelector(".cross").style.display="none"
+    }
+    else{
+        document.querySelector(".hem").style.display="none"
+        setTimeout(()=>{
+            document.querySelector(".cross").style.display="inline"
+        },400)
+       
+    }
+})
